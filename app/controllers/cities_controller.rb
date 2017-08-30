00f61cc2dde105f7faf50ef544a1527bd442fffa
Jess Gardner <inanna1130@gmail.com>
2017-08-30 13:31:20 -0400

    added new create and private

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,11 +1,15 @@
 class CitiesController < ApplicationController
   def index
+    @cities = City.all
   end
 
   def new
+    @city = City.new
   end
   
   def create
+    @city = City.create!(city_params)
+    redirect_to cities_path
   end
 
   def show
@@ -18,5 +22,10 @@ class CitiesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def city_params
+  params.require(:city).permit(:name, :description, :image)
   end
 end
