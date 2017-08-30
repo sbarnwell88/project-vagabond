1ddbc155c74a693600fb7002d28da39534e4e5b1
Jess Gardner <inanna1130@gmail.com>
2017-08-30 15:51:31 -0400

    created post show page and nested forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
   end
 
   def new
+    @city = City.find params[:city_id]
+    @post = Post.new
   end
   
   def create
+    @city = City.find params[:city_id]
+    @post = @city.posts.create!(post_params)
+    redirect_to city_path(@city)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -19,4 +26,10 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :tip)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
 
-  get 'posts/edit'
-
-  get 'posts/new'
-
-  get 'posts/show'
-
-  get 'posts/create'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
-  resources :cities
+  resources :cities do
+    resources :posts
+     end
   root to: "cities#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
