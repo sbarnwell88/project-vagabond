bf37fd2a2f56ea10a16fbaeec4bc11726624a39d
Jess Gardner <inanna1130@gmail.com>
2017-08-31 17:26:25 -0400

    user access through cancancan working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-
-
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:notice] = "Access denied!"
+    flash.keep(:notice)
+    redirect_to root_url
+  end
 
 
   protected
