00b7715caa5e2f1e55fde1eeb52df909457ae5a1
sbarnwell88 <susy6688@gmail.com>
2017-09-01 12:49:10 -0400

    added comments and fixed readme

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,6 @@
 class City < ApplicationRecord
     extend FriendlyId
+    # added friendly to make browser show city name
     friendly_id :name, use: :slugged
     
     has_many :posts, dependent: :destroy
