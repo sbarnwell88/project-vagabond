1e78b74f37f8ed865ce8d091a4f30ef6002364e3
Jess Gardner <inanna1130@gmail.com>
2017-09-01 01:25:23 -0400

    slight CSS to cover splash page. moved access alert to form page. logout redirect fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  rescue_from CanCan::AccessDenied do |exception|
-    flash[:notice] = "Access denied!"
-    flash.keep(:notice)
-    redirect_to root_url
-  end
+  before_action :store_current_location, unless: :devise_controller?   # save location for redirect after login/logout
 
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  private   
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)       # Overwriting the redirect path method
+    cities_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)       
+    request.referrer || root_path
   end
 end
